19c4da857b5d980768087ada90739e6ff15716a2
Nate-OBrien <159456288+Nate-OBrien@users.noreply.github.com>
2025-01-28 19:14:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 * Name: Nate O'Brien
 * Program: Vectors Lab 2
 * Date: 1/27/25
-* Extra: uses find to find the value they want
+* Extra: uses find to find the index of the value they want
 */
-
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -19,23 +20,41 @@ int main() {
 
     cout << "\n\nFavorite Foods: ";
     for (auto itr = foods.begin(); itr != foods.end(); ++itr) {
-        cout << *itr << ", ";
+        cout << *itr << (itr == foods.end() - 1 ? "." : ", "); // uses ternary to either print comma or period
     }
 
-
+    //gets input for the new food and the one to be replaced
     string food = "";
-    cout << "What food do you want to add? ";
-    cin >> food;
+    string foodInd = "";
+    cout << "\nWhat food do you want to add? ";
+    getline(cin, food);
+    cout << "\nWhat food do you want to add this to the left of? ";
+    getline(cin, foodInd);
     // using find, gets the index of the value
-    auto index = find(foods.begin(), foods.end(), food);
+    auto index = find(foods.begin(), foods.end(), foodInd);
     //adds the users food
-    foods.insert(foods.begin() + index, food);
-
-
-    cout << "\n\nFavorite Foods: ";
+    foods.insert(index, food);
+    
+    //reprints vector
+    cout << "\n\nUpdated Favorite Foods: ";
     for (auto itr = foods.begin(); itr != foods.end(); ++itr) {
-        cout << *itr << ", ";
+        cout << *itr << (itr == foods.end() - 1 ? "." : ", "); 
     }
+    
+    // gets input
+    string deleted = "";
+    cout << "\nWhat food is your least favorite? ";
+    getline(cin, deleted);
+    // gets the food to delete with find
+    auto deleteIndex = find(foods.begin(), foods.end(), deleted);
+    foods.erase(deleteIndex);
+    
+    //reprints vector
+    cout << "\n\nUpdated Favorite Foods: ";
+    for (auto itr = foods.begin(); itr != foods.end(); ++itr) {
+        cout << *itr << (itr == foods.end() - 1 ? "." : ", "); 
+    }
+    
    
     return 0;
 }
